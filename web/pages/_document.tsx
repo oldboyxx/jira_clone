ae3c1c29b3f720462d6d8a70558e974bdc52546d
Robert <robert.janaszek@gmail.com>
2022-05-01 00:06:26 +0200

    Attempt with rbd

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,11 +1,26 @@
-import { createGetInitialProps } from '@mantine/next';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import * as React from 'react';
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyles, createStylesServer } from '@mantine/next';
+import { resetServerContext } from 'react-beautiful-dnd';
 
-const getInitialProps = createGetInitialProps();
+const stylesServer = createStylesServer();
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    resetServerContext();
+    const initialProps = await Document.getInitialProps(ctx);
 
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={stylesServer} />
+        </>
+      ) as any, // TODO: fix typing
+    };
+  }
+  
   render() {
     return (
       <Html>
--- web/pages/project/board.tsx
+++ web/pages/project/board.tsx
@@ -1,5 +1,16 @@
 import { Badge, Card, Grid, Group, Paper, Text } from "@mantine/core";
 import * as React from "react";
+import { useEffect, useState } from "react";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { resetServerContext } from 'react-beautiful-dnd';
+
+const useFirstRender = () => {
+  const [isFirstRender, setFirstRender] = useState(true);
+  useEffect(() => {
+    setFirstRender(false);
+  }, [isFirstRender]);
+  return isFirstRender;
+}
 
 const Task = () => {
   return <Card mb="sm" sx={(theme) => ({
@@ -33,33 +44,61 @@ const StatusLane = ({ children, name }: React.PropsWithChildren<{ name: string }
 );
 
 const Board = () => {
-  return <Grid align="stretch" sx={() => ({ position: 'relative', })}>
-    <Grid.Col span={3}>
-      <StatusLane name="Backlog">
-        <Task />
-        <Task />
-        <Task />
-      </StatusLane>
-    </Grid.Col>
-    <Grid.Col span={3}>
-      <StatusLane name="Ready for development">
-        <Task />
-        <Task />
-        <Task />
-      </StatusLane>
-    </Grid.Col>
-    <Grid.Col span={3}>
-      <StatusLane name="In progress">
-        <Task />
-        <Task />
-      </StatusLane>
-    </Grid.Col>
-    <Grid.Col span={3}>
-      <StatusLane name="Done">
-        <Task />
-      </StatusLane>
-    </Grid.Col>
-  </Grid>;
+  // TODO: react-beautiful-dnd has issues when using with next.js
+  // https://github.com/atlassian/react-beautiful-dnd/issues/2092
+  // https://github.com/atlassian/react-beautiful-dnd/issues/1775
+  // This "fix" disables SSR in this component, but still doesn't work
+  const isFirstRender = useFirstRender();
+
+  if (isFirstRender) {
+    return null;
+  }
+
+  return <DragDropContext onDragEnd={() => console.log('drag end')}>
+    <Grid align="stretch" sx={() => ({ position: 'relative', })}>
+      <Grid.Col span={3}>
+        <Droppable droppableId="backlog" key="backlog">
+          {(dropProvided, dropSnapshot) => (
+            <div {...dropProvided.droppableProps} ref={dropProvided.innerRef}>
+              <StatusLane name="Backlog">
+                <Draggable draggableId="task-1234" index={0}>
+                  {(dragProvided, dragSnapshot) => (
+                    <div
+                      ref={dragProvided.innerRef}
+                      {...dragProvided.draggableProps}
+                      {...dragProvided.dragHandleProps}
+                    >
+                      <Task />
+                    </div>
+                  )}
+                </Draggable>
+                <Task />
+                <Task />
+              </StatusLane>
+            </div>
+          )}
+        </Droppable>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <StatusLane name="Ready for development">
+          <Task />
+          <Task />
+          <Task />
+        </StatusLane>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <StatusLane name="In progress">
+          <Task />
+          <Task />
+        </StatusLane>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <StatusLane name="Done">
+          <Task />
+        </StatusLane>
+      </Grid.Col>
+    </Grid>
+  </DragDropContext>;
 }
 
 export default Board;
